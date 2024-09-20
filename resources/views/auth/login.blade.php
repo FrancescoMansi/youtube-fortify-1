b29c7552bcc7c2ee2f2c717e4b852da483bf8417
francesco mansi <f.mansi@socialwebsolutions.it>
2024-09-20 11:49:36 +0200

    Init project

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,17 @@
+<h1>Pagina di Login</h1>
+@if ($errors->any())
+    <ul>
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+@endif
+<form action="{{ route('login') }}" method="post">
+    @csrf
+    <input type="text" name="email" id="email">
+    <input type="password" name="password" id="password">
+
+    <button type="submit">Accedi</button>
+</form>
+<hr>
+<a href="{{ route('register') }}">Non sei Registrato? Clicca qui</a>
